1ace6537634ff4ee2dd7fb4bdaa10f7a5277ef14
znkus1 <seongjin.eom00@gmail.com>
2023-05-01 14:43:15 +0900

    feat: daaaa

--- selector.py
+++ selector.py
@@ -1,4 +1,11 @@
 import random
 
-result = random.sample(range(1,45+1),k=6)
-print(result)
+
+def get_lucky_nums():
+    return random.sample(range(1,45+1), k=6)
+
+
+if __name__ == '__main__':
+    times = int(input('Enter num(1-100): '))
+    for _ in range(times):
+        print(get_lucky_nums())
