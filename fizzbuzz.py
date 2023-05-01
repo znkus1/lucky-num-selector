c2df38a771cad0514df5577fb3cacc80466b8f30
znkus1 <seongjin.eom00@gmail.com>
2023-05-01 15:29:42 +0900

    fis:i to j

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1,20+1):
-	if i%15==0:
+for j in range(1,16+1):
+	if j%15==0:
 		print('fizzbuzz')
-	elif i%3==0:
+	elif j%3==0:
 		print('fizz')
-	elif i%5==0:
+	elif j%5==0:
 		print('buzz')
 	else:
-		print(f'{i}')
+		print(f'{j}')
